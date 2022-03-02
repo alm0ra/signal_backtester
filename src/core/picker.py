b75d54a6d6006f985a8796e28b3a90fcfdf510fa
ali <ali.mrd318@gmail.com>
2022-03-02 22:31:17 +0330

    add : core picker strategies

---
+++ src/core/picker.py
@@ -0,0 +1,20 @@
+from src.strategies.one_sided import *
+from src.strategies.two_sided import *
+
+STRATEGIES = {
+    # two-sided strategies
+    'two_side_sl_tp_reversed': TwoSidedSlTpReverse,
+
+    # stop trailing
+    'two_side_sl_trailing_reversed': TwoSidedSlTrailingReverse,
+
+
+    # one-sided strategies
+    'one_side_buy_sl_tp': BuyOneSidedSlTpCloseOpposite,
+    'one_side_sell_sl_tp': SellOneSidedSlTpCloseOpposite,
+
+    # stop-trailing
+    'one_side_buy_sl_trailing': BuyOneSidedSlTrailingCloseOpposite,
+    'one_side_sell_sl_trailing': SellOneSidedSlTrailingCloseOpposite,
+}
+
