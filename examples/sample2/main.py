c44fe5d30cb3f1b9175ffe9fedaaba7fd687fae4
ali <ali.mrd318@gmail.com>
2022-03-04 22:41:14 +0330

    fix : examples

--- examples/sample2/main.py
+++ examples/sample2/main.py
@@ -6,11 +6,11 @@ dataset_address = "./final_dataset.csv"
 
 backtest = SignalBacktester(dataset=dataset_address,
                             strategy='two_side_sl_tp_reversed',
-                            cash=1000,
+                            cash=100000,
                             commission=0.0005,
                             percent_of_portfolio=99,
                             stop_loss=1,
-                            take_profit=10,
+                            take_profit=2,
                             trailing_stop=3,
                             output_path='./result'     # path of result files
                             )
